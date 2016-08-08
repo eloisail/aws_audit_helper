92187d82074c243bbb049ffc7bc8df5ed6cfb958
eloisail <eibanez@paradigmatecnologico.com>
2016-08-08 13:05:41 +0200

    csv formatted

--- aws_auditor.py
+++ aws_auditor.py
@@ -1,14 +1,6 @@
 # -*- encoding: utf-8 -*-
 import time
 import boto3
-print (r"-------¡Hola!")
-time.sleep(1)
-print (r"Este script usa Boto 3.")
-print (r"Antes de ejecutarlo se deben configurar las credenciales de autenticación para el cliente de aws.")
-print (r"Suelen estar en ~/.aws/credentials")
-print (r"Para más información: https://boto3.readthedocs.io/en/latest/guide/quickstart.html#configuration")
-print (r"-------")
-time.sleep(3)
 
 # Una forma de hacerlo sin el cliente
 # Directamente cogiendo los recursos
@@ -42,27 +34,27 @@ instances_size = len(instances_dict)
 
 subnets_dict = subnets_json['Subnets']
 subnets_size = len(subnets_dict)
+print("Vpc Tag,Vpc Id,Vpc Cidr Block,Instance Name,Instance Id,Instance Type,Public Ip Address,Private Ip Address,Security Group Name,Subnet Id,Availability Zone")
 
 for vpc in vpcs_dict:
-    print("--------Vpc-----------")
     if 'Tags' in vpc:
-        print(vpc['Tags'][0]['Value'] +" "+vpc['VpcId']+ " "+vpc['CidrBlock'])
+        print(vpc['Tags'][0]['Value'] +","+vpc['VpcId']+ ","+vpc['CidrBlock'])
     else:
-        print(vpc['VpcId']+" "+vpc['CidrBlock'])
-
-    print("----------------------")
+        print(" ,"+vpc['VpcId']+","+vpc['CidrBlock'])
 
     for instance in instances_dict:
-#        print instance['Instances']
-#        print instance['Instances'][0]['InstanceId']
         for inst in instance['Instances']:
             if (inst['VpcId'] == vpc['VpcId']):
-                print("    "+ "---------Instancias----------")
-                print("    " + inst['InstanceId'])
-                print("    " + inst['InstanceType'])
-                print("    " + inst['PublicIpAddress'])
-
-
-
-        
+                tags=inst['Tags']
+                for tag in tags:
+                    if (tag['Key']=='Name'):
+                        print(" , , ,"+tag['Value']+","+inst['InstanceId']+","+inst['InstanceType']+","+ inst['PublicIpAddress']+","+inst['PrivateIpAddress'])
+                        secGroupId = inst['SecurityGroups']
+                        for group in secGroupId:
+                            print(" , , , , , , , ,"+group['GroupName'])
+                            networkIf = inst['NetworkInterfaces']
+                            for network in networkIf:
+                                for subnet in subnets_dict:
+                                    if network['SubnetId'] == subnet['SubnetId']:
+                                        print(" , , , , , , , , ,"+network['SubnetId']+","+subnet['AvailabilityZone'])
 
